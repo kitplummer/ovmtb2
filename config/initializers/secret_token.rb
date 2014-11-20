9b7b102c05e8ea2d55eacd896aa15d63ebfa03ca
Kit Plummer <kitplummer@gmail.com>
2014-11-20 07:46:12 -0700

    Created application using Refinery CMS 2.1.5

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ovmtb2::Application.config.secret_token = '00b37f45cee6250aa8013ad5ca70b17cc2bed13987f12760e5c1dd354a8569c988ef5da67c902d70ab9aed61d552cec9f88b133bc2c5e084e149e6eb4fa43460'
